58b24d28bad4bd760085d31c051161e50ac383cb
013487 <000@>
2025-10-27 15:52:59 +0800

    测试

---
+++ src/main/java/com/dongbao/test_demo3/TestDemo3Application.java
@@ -0,0 +1,13 @@
+package com.dongbao.test_demo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDemo3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/dongbao/test_demo3/TestDemo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongbao.test_demo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDemo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
